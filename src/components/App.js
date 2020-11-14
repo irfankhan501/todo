60ec67003e71d72451b89faeaaba53100b8854fc
irfankhan501 <70253592+irfankhan501@users.noreply.github.com>
2020-11-14 14:48:15 +0530

    Update App.js
    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+window.id = 0;
+function App() {
+  const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [active, setActive] = useState(true);
 
+  function handleChange(e) {
+    if (value === "") {
+      setActive(false);
+	} 
+     setValue(e.target.value);
+   
+  }
+
+  function handleSave() {
+    if (value !== "") {
+      const newTasks = [...tasks, { key: window.id++, text: value }];
+      setTasks(newTasks);
+      setValue("");
+    }
+  }
+
+  function handleEdit(id) {
+    const newValue = tasks.filter((task) => task.key === id);
+    const newTasks = tasks.filter((task) => task.key !== id);
+    setTasks(newTasks);
+    setValue(newValue[0].text);
+  }
+
+  function handleDelete(id) {
+    const newTasks = tasks.filter((task) => task.key !== id);
+    setTasks(newTasks);
+  }
+  return (
+    <div id="main">
+      <textarea
+        id="task"
+        className="editTask"
+        onChange={handleChange}
+        value={value}
+      >
+        {value}
+      </textarea>
+      <button
+        id="btn"
+        className="saveTask"
+        onClick={() => handleSave()}
+        disabled={active}
+      >
+        Save
+      </button>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <div key={task.key}>
+              <li className="list">{task.text}</li>
+              <button className="edit" onClick={() => handleEdit(task.key)}>
+                Edit
+              </button>
+              <button className="delete" onClick={() => handleDelete(task.key)}>
+                delete
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
